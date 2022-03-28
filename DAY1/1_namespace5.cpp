d2e677e3d00eadc3b77d55b4a7ed63c3dedc5f78
ecourse <smkang@codenuri.co.kr>
2022-03-28 10:41:58 +0900

    Add files via upload

--- DAY1/1_namespace5.cpp
+++ DAY1/1_namespace5.cpp
@@ -9,9 +9,20 @@ namespace Audio
 {
 	using ::foo; // 이렇게 표기하는 문법이 있습니다.
 }
+using namespace Audio;
 
 int main()
 {
 	foo();
 	Audio::foo();
 }
+// <stdio.h> 안에 
+int printf(const char* fmt, ...); // 이 선언이  있습니다
+
+// <cstdio> 안에서. 
+#include <stdio.h>
+namespace std
+{
+	using ::printf;
+}
+using namespace std; // 이 코드가 있어도 결국 printf()는 한개 이므로 충돌없습니다.

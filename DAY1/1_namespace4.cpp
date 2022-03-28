e032c1b4985f1e07dab6f8c93607767f8357f9b6
ecourse <smkang@codenuri.co.kr>
2022-03-28 10:17:17 +0900

    Add files via upload

---
+++ DAY1/1_namespace4.cpp
@@ -0,0 +1,11 @@
+// 1_namespace4.cpp - 10 page
+#include <stdio.h>
+
+int main()
+{
+	// printf 는 C 표준 함수이기도 하고
+	// C++ 표준 이기도 합니다.
+
+	printf("hello\n");     // ok
+	std::printf("hello\n");// ??
+}

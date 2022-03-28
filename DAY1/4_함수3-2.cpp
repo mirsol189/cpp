7f5c1b96d1b69e3e28d11f7ce0164a040351bc9d
ecourse <smkang@codenuri.co.kr>
2022-03-28 15:42:04 +0900

    Add files via upload

--- DAY1/4_함수3-2.cpp
+++ DAY1/4_함수3-2.cpp
@@ -12,6 +12,16 @@ int main()
 {
 	int n = 3;
 		
-	int ret1 = SQUARE(n);
-	int ret2 = square(n);
+	int ret1 = SQUARE(n); // ((n)*(n)) 으로 소스 코드 레벨 치환
+	int ret2 = square(n); // 기계어 코드로 치환
+	
+	// 결국 위 2줄의 기계어 코드는 거의 동일합니다.
+	// 그런데, 매크로 함수는 안정성이 떨어집니다.
+	// n = 3
+	int ret3 = SQUARE(++n); // ((++n)*(++n))
+	n = 3;
+	int ret4 = square(++n);
+
+	std::cout << ret3 << std::endl; // ?
+	std::cout << ret4 << std::endl; //  예상대로 16 입니다.
 }

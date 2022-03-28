14a543bc9265988d187c940aa3b87e8f170af037
ecourse <smkang@codenuri.co.kr>
2022-03-28 17:16:06 +0900

    Add files via upload

--- DAY1/10_nullptr2.cpp
+++ DAY1/10_nullptr2.cpp
@@ -7,6 +7,16 @@ void foo(int* n)   { std::cout << "int*" << std::endl; }
 
 int main()
 {
-	foo(0); // 
-	foo(0);
+	foo(0); // int      "0 은 정수형 리터럴"
+	foo(0.0); // double "0.0 은 실수형 리터럴"
+	foo(false ); // bool "false는 bool형 리터럴"
+	foo(nullptr);  // int*  "nullptr 은 포인터형 리터럴"
+
+	// nullptr의 정체는 0 아닌가요 ?
+	// 아닙니다. 완전히 다릅니다.
+	std::nullptr_t a = nullptr; // nullptr_t 타입입니다.
+
+	// 이제 a는 nullptr 처럼 사용가능 합니다.
+	int* p2 = a; // ok!
+		
 }

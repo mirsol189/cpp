021961a02b88ee284b85973e584760ea7c6ad55a
ecourse <smkang@codenuri.co.kr>
2022-03-28 16:10:47 +0900

    Add files via upload

---
+++ DAY1/5_함수템플릿2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+template<typename T1, typename T2>
+? Add(T1 a, T2 b)
+{
+	return a + b;
+}
+
+int main()
+{
+	std::cout << Add<int, double>(1, 2.2) << std::endl; // 타입을 전달하는 경우
+	
+	std::cout << Add(1, 2.2) << std::endl; // 타입인자를 생략하는 경우
+											// 함수 인자로 타입 추론
+}
+

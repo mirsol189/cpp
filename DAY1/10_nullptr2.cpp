3e3f230aeb6eeaa95b7342316f0bd8d1c76bac5d
ecourse <smkang@codenuri.co.kr>
2022-03-28 17:11:09 +0900

    Add files via upload

---
+++ DAY1/10_nullptr2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+void foo(int n)    { std::cout << "int" << std::endl; }
+void foo(double n) { std::cout << "double" << std::endl; }
+void foo(bool n)   { std::cout << "bool" << std::endl; }
+void foo(int* n)   { std::cout << "int*" << std::endl; }
+
+int main()
+{
+	foo(0); // 
+	foo(0);
+}

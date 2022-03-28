39dd7fe02069a99c2f719aac2cc3753e170adbf8
ecourse <smkang@codenuri.co.kr>
2022-03-28 12:00:11 +0900

    Add files via upload

--- DAY1/3_변수와타입4.cpp
+++ DAY1/3_변수와타입4.cpp
@@ -4,8 +4,10 @@
 //typedef void(*PF)();
 
 // C++11 부터는 typedef 보다 using 이 편리합니다.
+// typedef : 타입에 대한 별칭
+// usnig   : 타입에 대한 별칭 + 템플릿에 대한 별칭
 using DWORD = int;
-using PF = void(*)();
+using PF    = void(*)();
 
 int main()
 {

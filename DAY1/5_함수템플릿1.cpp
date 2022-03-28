6c3a281373d04b026324ffa1a8c08f4b889a5c9f
ecourse <smkang@codenuri.co.kr>
2022-03-28 16:07:38 +0900

    Add files via upload

---
+++ DAY1/5_함수템플릿1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+// 다양한 타입에 대해서 동작하는 함수가 필요하면
+// 틀(템플릿)을 만들면 된다.
+template<typename T> 
+T Add(T a, T b)
+{
+	return a + b;
+}
+
+int main()
+{
+	std::cout << Add(1,   2 ) << std::endl;
+	std::cout << Add(1.1, 2.2) << std::endl;
+
+	std::cout << Add(1, 2.2) << std::endl; // error
+									// 컴파일러가 T 타입을 결정할수없다.
+
+	std::cout << Add<double>(1, 2.2) << std::endl; // ok
+
+}
+

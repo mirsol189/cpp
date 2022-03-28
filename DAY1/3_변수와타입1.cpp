64e5390eec00c270cb13a29d51851eb379aea54c
ecourse <smkang@codenuri.co.kr>
2022-03-28 11:13:40 +0900

    Add files via upload

---
+++ DAY1/3_변수와타입1.cpp
@@ -0,0 +1,18 @@
+// 3_변수와타입1.cpp - 18 ~ 19 
+#include <iostream>
+
+struct Point
+{
+	int x = 0;
+	int y = 0;
+};
+
+int main()
+{
+	struct Point p1; // C스타일 
+	Point p2; // C++ 스타일
+
+	int n1 = 10;
+	int n2 = 100000;
+
+}

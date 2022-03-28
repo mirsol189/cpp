5f7809fd95148ee38cd638f93f976142148c357e
ecourse <smkang@codenuri.co.kr>
2022-03-28 09:44:11 +0900

    Add files via upload

---
+++ DAY1/1_namespace1.cpp
@@ -0,0 +1,15 @@
+// 1_namespace1.cpp - 7page
+#include <stdio.h>
+
+void init()
+{
+	printf("init Audio Module\n");
+}
+void init()
+{
+	printf("init Video Module\n");
+}
+int main()
+{
+	init();
+}
---
+++ DAY1/hello.cpp
@@ -0,0 +1,11 @@
+// hello.cpp
+#include <stdio.h>
+
+int main()
+{
+	printf("hello\n");
+}
+
+// 컴파일 하는 방법 : ctrl + shift + B 를 누르거나
+
+// 컴파일 + 실행을 동시에 :  Ctrl + F5  로 하면 됩니다. 

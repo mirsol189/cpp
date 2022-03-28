133687c878768bed9cc55322a03c427db68bf1e7
ecourse <smkang@codenuri.co.kr>
2022-03-28 10:09:15 +0900

    Add files via upload

--- DAY1/1_namespace3.cpp
+++ DAY1/1_namespace3.cpp
@@ -1,9 +1,16 @@
 // 1_namespace3.cpp - 9 page
 #include <algorithm> // 이 안에 C++ 표준 min 함수가 있습니다.
+using namespace std;
 
 // 모든 C++ 표준은 "std" 라는 이름공간 안에 있습니다
+
+int count = 0;
 
 int main()
 {
 	int ret = min(2, 4);
+//	int ret = std::min(2, 4);
+
+	int n = count; // 왜 이코드가 에러 나올까요 ?? 
+				   // 에러메세지 확인해 보세요.
 }

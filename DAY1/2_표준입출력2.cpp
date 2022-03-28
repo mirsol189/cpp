6c172ac1aeb9c52582dde338a85ef94bf6310a20
ecourse <smkang@codenuri.co.kr>
2022-03-28 10:59:46 +0900

    Add files via upload

---
+++ DAY1/2_표준입출력2.cpp
@@ -0,0 +1,13 @@
+// 2_표준입출력2.cpp - 15 page 아래 부분~
+#include <iostream>
+
+int main()
+{
+	int n = 10;
+
+	std::cout << n << std::endl;
+
+	std::cout << n << std::endl;
+
+	std::cout << n << std::endl;
+}

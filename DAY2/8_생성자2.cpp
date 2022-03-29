5c6929bfb5ce1a83616d6f67cff4102f67302725
ecourse <smkang@codenuri.co.kr>
2022-03-29 15:04:57 +0900

    Add files via upload

---
+++ DAY2/8_생성자2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+class Point
+{
+	int x, y;
+public:
+	Point()             { x = 0; y = 0; std::cout << "Point()" << std::endl; }
+	Point(int a, int b) { x = a; y = b; std::cout << "Point(int, int)" << std::endl; }
+};
+
+class Rect
+{
+	Point ptFrom;
+	Point ptTo;
+public:
+	Rect() { std::cout << "Rect()" << std::endl; }
+};
+
+int main()
+{
+	Rect rc; // 화면 출력 결과 예측해 보세요.
+}
+
+

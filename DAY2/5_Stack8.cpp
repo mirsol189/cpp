39ceedef02d04309bfca8fe0e1b2d1a2fddaf65b
ecourse <smkang@codenuri.co.kr>
2022-03-29 14:03:12 +0900

    Add files via upload

--- DAY2/5_Stack8.cpp
+++ DAY2/5_Stack8.cpp
@@ -21,14 +21,17 @@ public:
 		delete[] buff; 
 
 	}
+//	void push(int value) {} // int 이므로 call by value
 
-	void push( const T& value) { buff[idx++] = value; }
+	void push(  const T& value) { buff[idx++] = value; }
 
 	T pop() { return buff[--idx]; }
 };
 
 int main()
 {
+	// 함수 템플릿 : 타입인자 생략시 "함수인자"로 타입추론
+	// 클래스 템플릿 : 반드시 타입인자 전달해야 합니다.
 	Stack<int> s1(20);
 	Stack<double> s2(20);
 

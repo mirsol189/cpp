5bab4965fd655329f1d3d54b8ddd910292466266
ecourse <smkang@codenuri.co.kr>
2022-03-29 14:34:39 +0900

    Add files via upload

---
+++ DAY2/5_Stack10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// 이미 C++ 표준에 템플릿 기반으로 만들어지 stack 이 있습니다.
+#include <stack>
+
+int main()
+{
+	std::stack<int> s; 
+
+	s.push(10);
+	s.push(20);
+
+	// 주의 ! 꺼내는 함수와 제거함수가 분리되어 있습니다.
+	std::cout << s.top() << std::endl; // 꺼내기만, 제거안됨 20 
+	std::cout << s.top() << std::endl; // 꺼내기만, 제거안됨 20
+
+	s.pop(); // 제거만, 반환은 안됨.
+	std::cout << s.top() << std::endl; // 10
+}
+

881eb1a30f997bc9abffe5d4be112636fcaddfb6
ecourse <smkang@codenuri.co.kr>
2022-03-29 11:12:28 +0900

    Add files via upload

---
+++ DAY2/5_Stack2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+// Step 2. push 함수가 전역변수를 사용하지 말고
+//		  buff 와 idx 를 인자로 전달 받자.
+
+// 장점 : 한개의 push, pop 함수로 2개이상의 스택을 사용할수 있다.
+// 단점 : 코드가 너무 복잡하다.
+
+void push(int* buff, int* idx, int value)
+{
+	buff[(*idx)++] = value;
+}
+int pop(int* buff, int* idx )
+{
+	return buff[--(*idx)];
+}
+int main()
+{
+	int buff1[10];
+	int idx1 = 0;
+
+	push(buff1, &idx1, 10);
+	push(buff1, &idx1, 20);
+	std::cout << pop(buff1, &idx1) << std::endl;
+}

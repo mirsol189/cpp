0935a5e312bffa09936783e5b018870c158633e4
ecourse <smkang@codenuri.co.kr>
2022-03-29 11:20:12 +0900

    Add files via upload

--- DAY2/5_Stack3.cpp
+++ DAY2/5_Stack3.cpp
@@ -1,13 +1,15 @@
 ﻿#include <iostream>
 
 // Step 3. "Stack" 이라는 타입을 설계 하자.
+// C 언어로 만들수 있는 최선의 코드(물론 다양한 테크닉도 가능하지만)
 
+// 단점. 상태를 나타내는 데이타와 "push, pop" 이 분리되어 있다.
+//     항상 push, pop 인자로 Stack* 가 필요하다.
 struct Stack
 {
 	int buff[10];
 	int idx;
 };
-
 void push(Stack* s, int value)
 {
 	s->buff[(s->idx)++] = value;
@@ -25,3 +27,13 @@ int main()
 	push(&s1, 20);
 	std::cout << pop(&s1) << std::endl;
 }
+
+
+
+
+
+
+
+
+
+

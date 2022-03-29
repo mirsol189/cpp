361fd953026858e28b4c608c880a8c64e41c4740
ecourse <smkang@codenuri.co.kr>
2022-03-29 17:11:51 +0900

    Add files via upload

--- DAY2/10_explicit5.cpp
+++ DAY2/10_explicit5.cpp
@@ -5,11 +5,15 @@ int main()
 {
 	std::vector<int> v1(10);			// 10개짜리 동적 배열
 	std::vector<int> v2 = { 1,2,3,4 };	// 4개짜리 동적배열
+						// vector(std::initializer_list) 입니다
 
 	// 아래 2줄의 차이점은 ?
-	std::vector<int> v3(10, 3);
-	std::vector<int> v4{ 10, 3 };
+	std::vector<int> v3(10, 3);		// vector(int, int) 생성자
+									// 10개를 3으로 초기화
 
-	std::cout << v3.size() << std::endl; // ?
-	std::cout << v4.size() << std::endl; // ?
+	std::vector<int> v4{ 10, 3 };	// vector(intializer_list)
+									// 2개를 10, 3으로 초기화
+
+	std::cout << v3.size() << std::endl; // 10
+	std::cout << v4.size() << std::endl; // 2
 }

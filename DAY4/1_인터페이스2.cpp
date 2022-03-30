ccfe6a5dad4b307566389d1cd449657684f3b7f2
ecourse <smkang@codenuri.co.kr>
2022-03-31 08:48:55 +0900

    Add files via upload

--- DAY4/1_인터페이스2.cpp
+++ DAY4/1_인터페이스2.cpp
@@ -6,6 +6,7 @@
 // 카메라 제작자가 지켜야하는 규칙을 먼저 설계한다.
 
 // 규칙 : "모든 카메라는 아래 클래스로 부터 파생되어야 한다."
+//			=> 모든 카메라는 take 함수를 만들어야 한다는 의미
 
 class ICamera
 {
@@ -13,36 +14,30 @@ public:
 	virtual void take() = 0; 
 };
 
+// 규칙이 있으므로 실제 카메라가 없어도 카메라를 사용하는 코드를
+// 먼저 작성해도 됩니다
+// 규칙대로만 사용하면 됩니다.
+class People
+{
+public:
+	void useCamera(ICamera* p) { p->take(); }
+};
 
+// 이제 모든 카메라는 "규칙"을 지켜야 합니다.
+// "ICamera" 로부터 파생되어야 합니다.
 
-
-
-
-
-
-
-
-
-class Camera
+class Camera : public ICamera
 {
 public:
 	void take() { std::cout << "take picture" << std::endl; }
 };
 
-class HDCamera
+class HDCamera : public ICamera
 {
 public:
 	void take() { std::cout << "take HD picture" << std::endl; }
 };
 
-
-
-class People
-{
-public:
-	void useCamera(Camera* p) { p->take(); }
-	void useCamera(HDCamera* p) { p->take(); }
-};
 
 int main()
 {

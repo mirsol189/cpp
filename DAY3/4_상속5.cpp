30eda93ed51ff0cd98c297e3f1d69f1d90b7aec2
ecourse <smkang@codenuri.co.kr>
2022-03-30 13:37:23 +0900

    Add files via upload

---
+++ DAY3/4_상속5.cpp
@@ -0,0 +1,20 @@
+// »ó¼Ó5.cpp
+#include <iostream>
+#include <string>
+
+class Person
+{
+	std::string name;
+	int age;
+};
+
+class Student : public Person
+{
+	int    id;
+};
+
+int main()
+{
+	Person p;
+}
+

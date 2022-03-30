39e2dbfac1d2a6c8c42dd370f350319b31c46d3b
ecourse <smkang@codenuri.co.kr>
2022-03-30 09:20:19 +0900

    Add files via upload

---
+++ DAY3/1_정적멤버5.cpp
@@ -0,0 +1,26 @@
+﻿
+// Car.h
+class Car
+{
+	int color;
+	static int cnt;
+public:
+	Car();
+	~Car();
+
+	static int get_count();
+};
+
+// Car.cpp
+#include "Car.h"
+
+int Car::cnt = 0; 
+
+Car::Car() { ++cnt; }
+Car::~Car() { --cnt; }
+
+// static 멤버 함수를 외부에 구현시 "static" 은 표기하지 않습니다.
+int Car::get_count() 
+{
+	return cnt; 
+}

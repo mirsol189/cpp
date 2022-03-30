e57e63c378341ab59b06b62178d6fd68aa409d53
ecourse <smkang@codenuri.co.kr>
2022-03-30 10:16:20 +0900

    Add files via upload

--- DAY3/2_this2_2.cpp
+++ DAY3/2_this2_2.cpp
@@ -14,11 +14,17 @@ Point f3()
 	return pt;
 }
 
+Point& f4()  // return by reference : 리턴용 임시객체를 만들지 말고
+{			 //						 pt 자체를 반환해 달라는 의미(alias반환)
+	return pt;
+}
+
 int main()
 {
-//	f3().x = 10;
-
-	f3();
+//	f3().x = 10; // error. 임시 객체
+//	f3();
+//	f4();
+	f4().x = 10; // ok..   pt.x = 10   참조 반환이므로 임시객체 아님.
 
 	std::cout << "-----" << std::endl;
 }

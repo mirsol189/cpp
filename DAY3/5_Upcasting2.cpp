199bcdf66af15d81d5773c5be0cd710ba3a5bb5d
ecourse <smkang@codenuri.co.kr>
2022-03-30 14:06:59 +0900

    Add files via upload

--- DAY3/5_Upcasting2.cpp
+++ DAY3/5_Upcasting2.cpp
@@ -22,6 +22,11 @@ int main()
 	std::vector<Animal*> v3; // 모든 동물을 보관하겠다는 의도..
 				// 동종(동일 기반 클래스 로 부터 파생된 타입)을 보관하는 컨테이너.
 
-	
+	// 아래 2개의 차이점을 많이 어려워 합니다.
+	std::vector<Animal>  v4;
+	std::vector<Animal*> v5;
 
+	Dog dog;
+	Animal* p = &dog; // 기존에 생성된 dog 를 가리키겠다는것
+	Animal  a = dog;  // 에러는 아닌데...
 }

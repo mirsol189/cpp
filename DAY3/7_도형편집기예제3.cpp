a43284a93a61ffce1b72f7ffad19b2dbae3e17b5
ecourse <smkang@codenuri.co.kr>
2022-03-30 15:42:22 +0900

    Add files via upload

--- DAY3/7_도형편집기예제3.cpp
+++ DAY3/7_도형편집기예제3.cpp
@@ -4,22 +4,32 @@
 // 핵심 3. 모든 도형(파생클래스)의 공통의 특징은 Shape(기반클래스)에도 
 //        있어야 한다. 그래야, Shape* 로 해당 특징을 사용할수 있다.
 
+// 핵심 4. 멤버 함수중, 파생 클래스가 재정의 하게 되는 멤버 함수는
+//		  반드시 가상함수로 해야 한다.
+
+
 class Shape
 {
+	int color;
 public:
-	void draw() { std::cout << "draw shape" << std::endl; }
+	void setColor(int c) { color = c; }
+	int getColor() const { return color; }
+
+	virtual void draw() { std::cout << "draw shape" << std::endl; }
 };
+
+
 
 class Rect : public Shape
 {
 public:
-	void draw() { std::cout << "draw rect" << std::endl; }
+	virtual void draw() { std::cout << "draw rect" << std::endl; }
 };
 
 class Circle : public Shape
 {
 public:
-	void draw() { std::cout << "draw circle" << std::endl; }
+	virtual void draw() { std::cout << "draw circle" << std::endl; }
 };
 
 int main()

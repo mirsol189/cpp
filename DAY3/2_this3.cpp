40f09d4760d99e86112cf4134f86de8dcf2c88ac
ecourse <smkang@codenuri.co.kr>
2022-03-30 10:34:17 +0900

    Add files via upload

---
+++ DAY3/2_this3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+class Counter
+{
+	int count = 0;
+public:
+	Counter increment()
+	{
+		++count;
+		return *this;
+	}
+	int get() { return count; }
+};
+
+int main()
+{
+	Counter c;
+//	c.increment();
+//	c.increment();
+//	c.increment();
+	c.increment().increment().increment();
+
+	std::cout << c.get() << std::endl;
+}

92642f4c5b95df13e0fee4ba53f018ab35e55f72
ecourse <smkang@codenuri.co.kr>
2022-03-30 15:08:04 +0900

    Add files via upload

---
+++ DAY3/7_도형편집기예제1.cpp
@@ -0,0 +1,33 @@
+﻿// 예제1     152 page
+#include <iostream>
+#include <vector>
+
+// 파워 포인트 같은 프로그램을 객체지향으로 만들어 봅시다.
+// 1. 모든 도형을 타입화 하면 편리합니다
+
+// 2. 모든 도형의 공통의 기반 클래스가 있다면 모든 도형을 
+//    하나의 vector에 보관할수 있다.
+
+class Shape
+{
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() { std::cout << "draw rect" << std::endl; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw circle" << std::endl; }
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+}
+
+
+

473efd6a639bd23b2fcb1d1a58dbcf15115b024c
ecourse <smkang@codenuri.co.kr>
2022-03-30 11:46:46 +0900

    Add files via upload

---
+++ DAY3/정리.cpp
@@ -0,0 +1,18 @@
+// 정리
+// C++에서 알아야 하는 것들
+
+// 1. C에도 있지만 C++에서 보다 발전된 것들 
+//  => auto, template, new, reference, namespace 등     1일차 내용
+
+// 2. 타입 한개를 만들때 필요 한 문법 - class 문법
+//  => private, public
+//  => 생성자, 소멸자
+//  => 멤버 초기화 리스트    Point(int a, int b) : x(a), y(b) {}
+//  => static 멤버 데이타, static 멤버 함수 
+//  => const member function : 상수 객체는 상수 멤버 함수만 호출가능
+//  => this 개념, this 를 반환하는 함수.
+//  => 함수가 참조(reference)를 반환하는 이유. 임시객체 
+
+//  => 객체의 복사 (복사 생성자)
+
+// 3. 상속과 객체지향 디자인... - 오늘부터 8~10시간정도

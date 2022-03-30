c584e04fd944f6f5124d786b98a263f34377f20f
ecourse <smkang@codenuri.co.kr>
2022-03-30 11:59:54 +0900

    Add files via upload

---
+++ DAY3/4_상속2.cpp
@@ -0,0 +1,25 @@
+﻿// 5_상속2
+
+class Base
+{
+private:   int a;	// 자신의 멤버 함수에서만 접근 가능
+protected: int b;	// 자신의 멤버 함수 + 파생클래스 멤버함수에서 접근 가능
+public:    int c;	// 어디서도 접근 가능
+};
+class Derived : public Base  
+{
+public:
+	void foo()
+	{
+		a = 0;	// error. 물려는 받지만, 접근은 안됨. 접근하려면 기반클래스 함수호출
+		b = 0;  // ok.. protected 는 파생 클래스에서도 접근 가능
+		c = 0;  // ok
+	}
+};
+int main()
+{
+	Base base;
+	base.a = 0; // error. private
+	base.b = 0; // error. protected는 외부 접근 안됨
+	base.c = 0; // ok
+}

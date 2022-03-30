d21105fac4274106358858c838a0f308b169f8d8
ecourse <smkang@codenuri.co.kr>
2022-03-30 15:55:58 +0900

    Add files via upload

---
+++ DAY3/7_도형편집기예제5.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+
+
+
+class Shape
+{
+	int color;
+public:
+	void setColor(int c) { color = c; }
+	int getColor() const { return color; }
+
+	virtual void draw() { std::cout << "draw shape" << std::endl; }
+};
+
+
+
+class Rect : public Shape
+{
+public:
+	virtual void draw() { std::cout << "draw rect" << std::endl; }
+};
+
+class Circle : public Shape
+{
+public:
+	virtual void draw() { std::cout << "draw circle" << std::endl; }
+};
+
+class Triangle : public Shape
+{
+public:
+	virtual void draw() { std::cout << "draw triangle" << std::endl; }
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd == 1)
+		{
+			v.push_back(new Rect);
+		}
+		else if (cmd == 2)
+			v.push_back(new Circle);
+
+		else if (cmd == 9)
+		{
+			for (auto p : v)
+				p->draw();   
+		}
+
+		else if (cmd == 8)
+		{
+			std::cout << "몇번째 도형을 복제 할까요 >> ";
+
+			int k;
+			std::cin >> k;
+
+			// k 번째 만든 도형의 복제본을 만들어서 v 끝에 추가합니다.
+			// 그런데, k 번째 만든 도형이 뭘까요 ?
+			// 어떻게 해야 할까요 ?
+		}
+	}
+}
+

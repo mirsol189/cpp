ad399f78bd6ccc7770fdf2699d838a075b173859
ecourse <smkang@codenuri.co.kr>
2022-03-31 15:56:30 +0900

    Add files via upload

---
+++ DAY4/8_연산자재정의3.cpp
@@ -0,0 +1,44 @@
+//  5_연산자재정의 - 182(131) page
+#include <iostream>
+
+class Point
+{
+	int x, y;
+public:
+	Point(int x = 0, int y = 0) : x(x), y(y) {}
+
+	void print() const { std::cout << x << ", " << y << std::endl; }
+
+/*
+	Point operator+(const Point& pt)
+	{
+		std::cout << "멤버로 만든 operator+" << std::endl;
+		Point temp(x + pt.x, y + pt.y);
+		return temp;
+	}
+*/
+};
+// 멤버가 아닌 operator+ 함수
+Point operator+(const Point& p1, const Point& p2)
+{
+	std::cout << "멤버가 아닌 operator+" << std::endl;
+
+	Point temp(p1.x + p2.x, p1.y + p2.y);
+	return temp;
+}
+
+
+
+int main()
+{
+	Point p1(1, 1);
+	Point p2(2, 2);
+
+	Point p3 = p1 + p2; // 1) p1.operator+(p2) 가 가능하면 사용
+						// 2) operator+(p1, p2) 가 가능하면 사용
+						// 1, 2 가 모두 실패하면 컴파일 에러.
+
+	p3.print();
+
+}
+

f627ec14f23ea80f573295732cfaacfb84bd753a
ecourse <smkang@codenuri.co.kr>
2022-03-31 09:42:18 +0900

    Add files via upload

--- DAY4/3_Edit예제2.cpp
+++ DAY4/3_Edit예제2.cpp
@@ -4,12 +4,23 @@
 
 // Validation 정책은 교체 가능해야 한다.
 
-// 방법 1. 
+// 공통성과 가변성의 분리
+// => 변하지 않은 코드 내부에 있는 변해야 하는 부분을 찾는다.
+// => 변해야 하는 부분을 분리한다.
+
+// 변하는 것을 분리하는 방법 1. 변하는 것을 가상함수로 분리한다.
+//							==> 변경하고 싶다면 파생 클래스를 설계하라는것
 
 class Edit
 {
 	std::string data;
 public:
+
+	// 변하는 것을 담은 가상함수
+	virtual bool validate(char c)
+	{
+		return true;
+	}
 
 	std::string getData()
 	{
@@ -22,7 +33,7 @@ public:
 			if (c == 13)      
 				break;
 
-			if (isdigit(c))
+			if (validate(c)) // 가상함수 호출
 			{
 				std::cout << c;    
 				data.push_back(c); 
@@ -34,13 +45,21 @@ public:
 	}
 };
 
-
-
-
+// validation 정책을 변경하려면
+// 파생 클래스를 만들어서 가상함수를 재정의 하라는 의도!
+class NumEdit : public Edit
+{
+public:
+	bool validate(char c) override
+	{
+		return isdigit(c);
+	}
+};
 
 int main()
 {
-	Edit e;
+	//Edit e;
+	NumEdit e;
 	while (1)
 	{
 		std::cout << e.getData() << std::endl;

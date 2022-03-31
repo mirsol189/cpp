71f16687cc2739e62319efe9639ca939fbb0ed3a
ecourse <smkang@codenuri.co.kr>
2022-03-31 14:18:24 +0900

    Add files via upload

--- DAY4/5_RTTI1.cpp
+++ DAY4/5_RTTI1.cpp
@@ -6,8 +6,31 @@ int main()
 
 	// 특정 변수의 타입을 알고 싶으면 "typeid" 라는 연산자를
 	// 사용하면 됩니다.
+	// typeid()의 반환 타입은 type_info 객체인데.. 
+	// 반드시 const & 로 받아야 합니다.
 	const std::type_info& t = typeid(n);
 
 	std::cout << t.name() << std::endl;
+
+
+	double* p = reinterpret_cast<double*>(&n);
+
+	std::cout << typeid(p).name() << std::endl; // double*
+	std::cout << typeid(*p).name() << std::endl; // double
+								// 즉, 실제로 따라가서 조사하는것은 아니고
+								// double* 이므로 * 하면 double 이겠지 !!
+								// 라고 컴파일 시간에 판단
+
+	// 타입이름을 출력하는 것이 아니라 조사하려면
+	const std::type_info& t1 = typeid(n);   // typeid(변수)
+	const std::type_info& t2 = typeid(int); // typeid(타입)
+
+	if (t1 == t2) {}
+
+	// 실전에서는 그냥 아래 처럼 하세요
+	if (typeid(n) == typeid(int)) //!!
+	{
+
+	}
 
 }

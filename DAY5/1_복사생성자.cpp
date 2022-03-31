16f0936ee319028826cdeb3427fff1392ffce638
ecourse <smkang@codenuri.co.kr>
2022-04-01 08:40:29 +0900

    Add files via upload

--- DAY5/1_복사생성자.cpp
+++ DAY5/1_복사생성자.cpp
@@ -22,8 +22,10 @@ public:
 int main()
 {
 	Point p1;		// 1번 생성자 사용
-	Point p2(1, 1);	// 2번 생성자 사용	
+	Point p2(2, 1);	// 2번 생성자 사용	
 //	Point p3(1);	// error. Point(int) 모양의 생성자는 없다.
 	Point p4(p2);   // ok..   Point(Point) 모양의 생성자가 필요
 					// 사용자가 만들지 않았지만, 컴파일러가 제공.
+
+	Point p5 = p2; // 역시 복사 생성자 호출.
 }

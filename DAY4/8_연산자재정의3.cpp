ed5172aa419a671e14b6be3a5d4fadce15396dff
ecourse <smkang@codenuri.co.kr>
2022-03-31 16:13:14 +0900

    Add files via upload

--- DAY4/8_연산자재정의3.cpp
+++ DAY4/8_연산자재정의3.cpp
@@ -38,5 +38,32 @@ int main()
 
 	p3.print();
 
+	// 연산자 재정의시
+	// 멤버 함수와 멤버가 아닌 함수중 어느것이 좋은가요 ?
+
+	// 1. "멤버로 하자." 주장의 근거 : private 데이타 접근은 멤버가 편하다
+
+	// 2. "일반함수로 하자." 주장의 근거 : 멤버 함수로 만들수 없을때가 있다.
+
+	// 아래 코드를 "멤버함수"로 만드는 것을 생각해 보세요
+	int n = 3;
+
+	Point p4 = p1 + p2; // p1.operator+(Point) 가 있으면 ok
+	Point p5 = p1 + n;  // p1.operator+(int)   가 있으면 ok
+	Point p6 = n  + p1; // n.operator+(Point)  가 있으면 되는데.. 
+					    // 그런데, n는 user type 이 아니므로 멤버함수가없다.
+						// 그런데, 멤버가 아닌 함수로는 만들수 있다.
+						// operator+(int, Point) 로 하면 된다.
+
+	// 파이썬의 철학이 재미있습니다.
+//	p6 = p1 + n   // p1.__add__(Int)
+
+//	p7 = n + p1   // 1. n.__add__(Point) 이 가능하면 사용
+				  // 2. p1.__radd__(Int) 를 사용
 }
 
+// C#, java : 멤버 함수 개념만 존재..
+// swift, python : 멤버 함수, 일반 함수 모두 존재
+
+// python : private 개념이 없으므로 모든 일반 함수에서 멤버 접근 가능
+// swift  : 일반함수에서 private 멤버 접근 안됨.

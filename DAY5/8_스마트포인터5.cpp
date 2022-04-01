bc58484ed10e0b556b4c2b00c09539e77ad7132d
ecourse <smkang@codenuri.co.kr>
2022-04-01 10:45:32 +0900

    Add files via upload

--- DAY5/8_스마트포인터5.cpp
+++ DAY5/8_스마트포인터5.cpp
@@ -5,15 +5,18 @@
 struct Person
 {
 	std::string name;
+	std::shared_ptr<Person> bestFriend;
 
 	Person(const std::string& n) : name(n) {}
-	~Person() { std::cout << name << " 파괴" << std::endl; }
-
-	std::shared_ptr<Person> bestFriend;
+	~Person() { std::cout << name << " 파괴" << std::endl; }	
 };
 
 int main()
 {
-	std::shared_ptr<Person> p1(new Person("kim"));
-	std::shared_ptr<Person> p2(new Person("lee"));
+	std::shared_ptr<Person> p1( new Person("kim") );
+	std::shared_ptr<Person> p2( new Person("lee") );
+
+	// 아래 같은 현상을 상호참조(원형참조, cycle reference)라고 합니다.
+	p1->bestFriend = p2;
+	p2->bestFriend = p1;
 }

0b002fe95964c3a215e3bc78c1952e68276f553a
ecourse <smkang@codenuri.co.kr>
2022-04-01 09:20:15 +0900

    Add files via upload

--- DAY5/2_객체복사3.cpp
+++ DAY5/2_객체복사3.cpp
@@ -4,7 +4,8 @@
 class People
 {
 	char* name;
-	int*  ref;  // 참조계수 관리를 위한 멤버
+//	int*  ref;  // 참조계수 관리를 위한 멤버
+	static int ref;
 	int   age;
 public:
 	People(const char* n, int a) : age(a)
@@ -15,10 +16,6 @@ public:
 		ref = new int;
 		*ref = 1;
 	}
-
-
-
-
 
 	// 아래 복사 생성자가 참조 계수로 구현한 복사 생성자 입니다.
 	People(const People& p) : name(p.name), ref(p.ref), age(p.age)
@@ -40,10 +37,18 @@ public:
 
 int main()
 {
+	// p1, p2, p3 는 같은 자원 공유
 	People p1("kim", 20);
 	People p2 = p1;
 	People p3 = p1;
-	People p4 = p1;
+
+	// p4, p5는 p1, p2, p3 와는 다른 자원 공유..
+	People p4("lee", 20);
+	People p5 = p4;
+
+	// 그런데, 참조 계수를 static 멤버로 하면
+	// p1, p2, p3, p4, p5 가 모두 공유하게 됩니다.
+	// 그래서, 참조계수를 static 으로 하면 안되고, 동적할당할수 밖에 없습니다.
 }
 
 

4089fdae74caa724108a67d72ccd8ad5d82ee702
ecourse <smkang@codenuri.co.kr>
2022-04-01 15:59:25 +0900

    Add files via upload

---
+++ DAY5/언어의특징.cpp
@@ -0,0 +1,17 @@
+// 언어의 특징
+int main()
+{
+	// C++
+	// primitive type과 user type type이 구분해서 사용.
+	int n = 3;
+	bool b = n.isMultiply(3); // error. int 는 primitive 타입이다.
+
+
+	// C# 의 등장 : "모든 것은 객체이다." 
+	//				java 까지는 primitive 타입 개념이 있음.(int 멤버함수없음)
+	
+	String s = 10.ToString(); // C# 은 가능.
+
+	// C, C++, java : int 는 멤버 함수 없음
+	// Python, C#, swift, kotlin 등 : int도 멤버 함수 있음.
+}
---
+++ DAY5/언어특징2.cpp
@@ -0,0 +1,14 @@
+
+// 메모리 이야기
+// C++
+class Point
+{
+	int x;
+	int y;
+};
+int main()
+{
+	Point pt; // 스택에 만든다
+	Point* p = new Point;
+
+}

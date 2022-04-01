7e9534c32b490a295d17368a30817d8d435cad0c
ecourse <smkang@codenuri.co.kr>
2022-04-01 10:50:33 +0900

    Add files via upload

--- DAY5/8_스마트포인터5.cpp
+++ DAY5/8_스마트포인터5.cpp
@@ -19,4 +19,8 @@ int main()
 	// 아래 같은 현상을 상호참조(원형참조, cycle reference)라고 합니다.
 	p1->bestFriend = p2;
 	p2->bestFriend = p1;
+
+	// shared_ptr 같은 참조계수 기반 스마트 포인터 사용시
+	// 위와 같은 상호 참조(원형참조)가 발생하면
+	// 자원삭제가 되지 않는 문제가 있습니다.
 }

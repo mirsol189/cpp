31ea862bc2d5bb55e3d8c4469e1b446eecc2624b
ecourse <smkang@codenuri.co.kr>
2022-04-01 12:06:04 +0900

    Add files via upload

---
+++ DAY5/7_예외6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+class file_not_found {};
+
+void foo()
+{
+	throw file_not_found();
+}
+void goo()
+{
+	try
+	{
+		foo();
+	}
+	catch (...)
+	{
+		throw; // 현재 발생된 예외를 다시 발생
+	}
+}
+int main()
+{
+	try
+	{
+		goo();
+	}
+	catch (...)
+	{
+
+	}
+}

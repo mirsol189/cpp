15da44d026c64881dbeefd0c97809eecda8d9760
ecourse <smkang@codenuri.co.kr>
2022-04-01 16:09:14 +0900

    Add files via upload

--- DAY5/언어특징2.cpp
+++ DAY5/언어특징2.cpp
@@ -1,4 +1,3 @@
-
 // 메모리 이야기
 // C++
 class Point
@@ -9,6 +8,30 @@ class Point
 int main()
 {
 	Point pt; // 스택에 만든다
-	Point* p = new Point;
+	Point* p = new Point; // 힙에 만든다 
 
+}
+
+// C#, java, swift 
+// => 타입의 메모리 위치를 "타입의 설계자가 결정"
+// struct : value type, 스택에 생성. 복사시 새로운 객체 생성
+// class  : reference type, 힙에생성, 복사시 참조(포인터 복사)
+class CPoint
+{
+	int x;
+	int y;
+};
+struct SPoint
+{
+	int x;
+	int y;
+};
+int main()
+{
+	// C#
+	CPoint cp = new CPoint();
+	SPoint sp = new SPoint();
+
+	SPoint sp2 = sp;
+	CPoint cp2 = cp;
 }
